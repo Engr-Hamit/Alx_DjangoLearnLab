ae8b93fc49386316d3f22ec44e2fcecbcf769b38
Engr-Hamit <hamitgadar05@gmail.com>
2025-08-10 15:33:37 +0200

    Setup Django project with api app, models, serializers

--- api/models.py
+++ api/models.py
@@ -1,23 +1,3 @@
 from django.db import models
 
-# Author model represents a book's author.
-class Author(models.Model):
-    name = models.CharField(max_length=255)  # Author's full name
-
-    def __str__(self):
-        return self.name
-
-
-# Book model represents a book and links to an Author.
-class Book(models.Model):
-    title = models.CharField(max_length=255)  # Book's title
-    publication_year = models.IntegerField()  # Year the book was published
-    author = models.ForeignKey(
-        Author,
-        related_name='books',  # Allows reverse access: author.books.all()
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.title
-
+# Create your models here.
